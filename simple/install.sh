77513df99926371f48d949f212e0b78094a17892
chrctr1101 <hazovsvatoslav@gmail.com>
2026-08-07 10:27:11 +0000

    Created script to copy config files

--- simple/install.sh
+++ simple/install.sh
@@ -1,3 +1,22 @@
 #!/bin/sh
 
-sudo pacman -S kitty waybar hyprpaper hyprlock fastfetch ttf-font-awesome otf-font-awesome ttf-jetbrains-mono noto-fonts noto-fonts-emoji
+RED='\033[38;5;196m'
+ORANGE='\033[38;5;208m'
+GREEN='\033[0;032m'
+BLUE='\033[0;034m'
+NC='\033[0m'
+
+
+echo -e "${BLUE}Start installing! You have 3 secounds to interrupt it!${NC}"
+
+sleep 5s
+echo -e "${RED}Warning! Please don't interrupt this script!\n${NC}"
+
+for DIR in "fastfetch" "hypr" "kitty" "waybar" "wlogout" "wofi"; do
+	sleep 0.1s
+	cp -r ${DIR} ~/config/
+	echo -e "${GREEN}${DIR} is copyed to your config-file"
+	sleep 0.1s
+done
+
+echo -e "${BLUE}\nEnd script! All is done!\n"
