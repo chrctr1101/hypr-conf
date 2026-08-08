d9b01e09051726bc889179e5df1097439bf259ab
chrctr1101 <hazovsvatoslav@gmail.com>
2026-08-08 19:36:14 +0300

    Added anime (bright red) installer + extended help

--- install.sh
+++ install.sh
@@ -42,14 +42,36 @@ elif [ "$1" = "--gotic" ]; then
 	sleep 1s
 	hyprctl dispatch 'hl.dsp.exit()'
 	
+elif [ "$1" = "--anime" ]; then
+	if [ "$2" = "bright_red"]; then
+		echo -e "${BLUE}Start installing 'Anime (Bright Red)'! You have 3 secounds to interrupt it!${NC}"
+		sleep 5s
+		echo -e "${RED}Warning! Please don't interrupt this script!\n${NC}"
+		cd ./anime/b_red/	
+
+		for DIR in "fastfetch" "hypr" "kitty" "waybar" "wlogout" "wofi"; do
+			sleep 0.1s
+			cp -r ${DIR} ~/.config/
+			echo -e "${GREEN}${DIR} is copyed to your config-file"
+			sleep 0.1s
+		done
+	
+		echo -e "${BLUE}\nAll is done!\nYour Hyprland session will reboot now!"
+		sleep 1s
+		hyprctl dispatch 'hl.dsp.exit()'
+	fi
+
 
 
 elif [ "$1" = "--help" ]; then
 	echo -e "\n${BLUE} Help${NC}"
-	echo -e "How to use: ./install.sh  [CONFIGURATION]\n\n"
+	echo -e "How to use: ./install.sh  [CONFIGURATION] {COLOR}\n\n"
 	echo -e "Configurations:\n"
 	echo -e "'--simple' -- simple view, like now-installed hyprland"
 	echo -e "'--gotic'  -- gotic view, grey and sad"
+
+	echo -e "'--anime' -- anime view, there are some types of colors
+	'b_red' -- bright red"
 
 
 	echo -e "'--help' -- to show this help\n"
