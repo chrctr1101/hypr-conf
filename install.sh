9c4aac7eb5543f2791ad8def5756409d5f106422
chrctr1101 <hazovsvatoslav@gmail.com>
2026-08-10 18:37:56 +0300

    Added new config (hyprchan)

--- install.sh
+++ install.sh
@@ -60,6 +60,25 @@ elif [ "$1" = "--arch" ]; then
 	sleep 1s
 	awww img ~/.config/hypr/images/bg1.jpg
 	hyprctl dispatch 'hl.dsp.exit()'
+	
+elif [ "$1" = "--hyprchan" ]; then
+	echo -e "${BLUE}Start installing 'Hypr-chan'! You have 3 secounds to interrupt it!${NC}"
+	sleep 5s
+	echo -e "${RED}Warning! Please don't interrupt this script!\n${NC}"
+	cd ./hyprchan/	
+
+	for DIR in "fastfetch" "hypr" "kitty" "waybar" "wlogout" "wofi"; do
+		sleep 0.1s
+		cp -r ${DIR} ~/.config/
+		echo -e "${GREEN}${DIR} is copyed to your config-file"
+		sleep 0.1s
+	done
+	
+	echo -e "${BLUE}\nAll is done!\nYour Hyprland session will reboot now!"
+	sleep 1s
+	awww img ~/.config/hypr/images/bg1.jpg
+	hyprctl dispatch 'hl.dsp.exit()'
+
 
 elif [ "$1" = "--anime" ]; then
 	if [ "$2" = "b_red" ]; then
@@ -87,9 +106,10 @@ elif [ "$1" = "--help" ]; then
 	echo -e "\n${BLUE} Help${NC}"
 	echo -e "How to use: ./install.sh  [CONFIGURATION] {COLOR}\n\n"
 	echo -e "Configurations:\n"
-	echo -e "'--simple' -- simple view, like just installed hyprland"
-	echo -e "'--gotic'  -- gotic view, grey and sad"
-	echo -e "'--arch'   -- arch-tyan, she'll look at you)"
+	echo -e "'--simple'   -- simple view, like just installed hyprland"
+	echo -e "'--gotic'    -- gotic view, grey and sad"
+	echo -e "'--arch'     -- arch-tyan, she'll look at you)"
+	echo -e "'--hyprchan' -- hypr-chan, official hyprland mascot"
 
 	echo -e "'--anime' -- anime view, there are some types of colors
 	'b_red' -- bright red"
