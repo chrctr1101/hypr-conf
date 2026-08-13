305eab3eeca372ece7317603a30fe8d9e5b6b3d1
chrctr1101 <hazovsvatoslav@gmail.com>
2026-08-13 14:57:19 +0300

    UPDATE. Added miku configuration

--- install.sh
+++ install.sh
@@ -79,6 +79,25 @@ elif [ "$1" = "--hyprchan" ]; then
 	awww img ~/.config/hypr/images/bg1.jpg
 	hyprctl dispatch 'hl.dsp.exit()'
 
+elif [ "$1" = "--miku" ]; then
+	echo -e "${BLUE}Start installing 'Miku'! You have 3 secounds to interrupt it!${NC}"
+	sleep 5s
+	echo -e "${RED}Warning! Please don't interrupt this script!\n${NC}"
+	cd ./miku/	
+
+	for DIR in "fastfetch" "hypr" "kitty" "waybar" "wlogout" "wofi" "fish"; do
+		sleep 0.1s
+		cp -r ${DIR} ~/.config/
+		echo -e "${GREEN}${DIR} is copyed to your config-file"
+		sleep 0.1s
+	done
+	
+	echo -e "${BLUE}\nAll is done!\nYour Hyprland session will reboot now!"
+	sleep 1s
+	awww img ~/.config/hypr/images/bg1.jpg
+	hyprctl dispatch 'hl.dsp.exit()'
+
+
 
 elif [ "$1" = "--anime" ]; then
 	if [ "$2" = "b_red" ]; then
@@ -106,16 +125,17 @@ elif [ "$1" = "--help" ]; then
 	echo -e "\n${BLUE} Help${NC}"
 	echo -e "How to use: ./install.sh  [CONFIGURATION] {COLOR}\n\n"
 	echo -e "Configurations:\n"
-	echo -e "'--simple'   -- simple view, like just installed hyprland"
-	echo -e "'--gotic'    -- gotic view, grey and sad"
-	echo -e "'--arch'     -- arch-tyan, she'll look at you)"
-	echo -e "'--hyprchan' -- hypr-chan, official hyprland mascot"
+	echo -e "'--simple'     -- simple view, like just installed hyprland"
+	echo -e "'--gotic'      -- gotic view, grey and sad"
+	echo -e "'--arch'       -- arch-tyan, she'll look at you)"
+	echo -e "'--hyprchan'   -- hypr-chan, official hyprland mascot"
+	echo -e "'--miku        -- Miku, famous vokaloid in the world'"
 
-	echo -e "'--anime' -- anime view, there are some types of colors
-	'b_red' -- bright red"
+	echo -e "'--anime'      -- anime view, there are some types of colors:\n
+	'b_red'         -- bright red"
 
 
-	echo -e "'--help' -- to show this help\n"
+	echo -e "\n'--help'       -- to show this help\n"
 else
 	echo -e "${RED}Incorret config input!\nType './install.sh --help' and select correct argument${NC}"
 fi
